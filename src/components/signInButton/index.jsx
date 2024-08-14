13b019daa753eccc4f86cf3100003596f56f008d
Oleksandr Zhurba <a.zhurbinho@gmail.com>
2024-08-14 22:46:37 +0200

    clean up

--- src/components/signInButton/index.jsx
+++ src/components/signInButton/index.jsx
@@ -1,7 +1,6 @@
-import styles from "./styles.module.css";
 export default function SignInButton({ id, handleHover, iconUrl }) {
   return (
-    <div>
+    <div style={{ userSelect: "none" }}>
       <img
         id={id}
         onMouseEnter={(event) => handleHover(event.target.id)}
