436bb6da428d038d1022c83fd7c440ca98162e8e
Oleksandr Zhurba <a.zhurbinho@gmail.com>
2024-08-15 20:31:36 +0200

    added icon lift and animation

--- src/components/signInButton/index.jsx
+++ src/components/signInButton/index.jsx
@@ -1,6 +1,7 @@
+import styles from "./styles.module.css";
 export default function SignInButton({ id, handleHover, iconUrl }) {
   return (
-    <div style={{ userSelect: "none" }}>
+    <div className={styles.button_wrapper}>
       <img
         id={id}
         onMouseEnter={(event) => handleHover(event.target.id)}
